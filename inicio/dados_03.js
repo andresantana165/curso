f0090c2ff236271fe58c0a4049ad873850950fa0
andre santana <andreesantana165@gmail.com>
2023-06-25 11:56:55 -0300

    curso

---
+++ inicio/dados_03.js
@@ -0,0 +1,12 @@
+let idade = 22;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estachovendo = true // ou false /boolean significa verdadeiro ou falso
+console.log(typeof estachovendo);
+
+console.log(typeof "teste");
+console.log(typeof 'texto');
---
+++ inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// pi * raio * raio
+
+const pi = 3.141592;
+const raio = 10;
+let areacirc = pi * raio * raio;
+
+console.log("o valor da area é " + areacirc + 'm2.');
+
+
+areacirc = Math.PI * raio * raio;
+console.log("o valor da area é " + areacirc + 'm2.' ) 
